dab704a8d9b75d042275180d50f026cb4e18de87
Liam <Liam@DESKTOP-5VP63DL>
2022-01-26 10:02:48 +0100

    (26-01-22) Subida del proyecto

---
+++ src/azofraliam/Azofra.java
@@ -0,0 +1,17 @@
+package azofraliam;
+
+/**
+ * 
+ * @author Liam
+ */
+
+
+public class Azofra {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hola");
+        System.out.println("Mundo");
+    }
+
+}
