25dc00fa072cc9fc74fd8c7a3207666b15cc0613
Liam <Liam@DESKTOP-5VP63DL>
2022-01-27 09:38:44 +0100

    Version 1

---
+++ src/azofraliam/Liam.java
@@ -0,0 +1,14 @@
+package azofraliam;
+
+/**
+ *
+ * @author Liam
+ */
+public class Liam {
+
+    public Liam() {
+        System.out.println("Prueba");
+        System.out.println("Rama");
+    }
+    
+}
